793b2cdacb2ce9b082d09db4876436679b9c1ccc
Iago Viotti <iago.viotti@gmail.com>
2024-11-01 11:30:48 -0300

    :sparkles: feat. gsap animation on aside

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -2,6 +2,7 @@ import { Home, User, Briefcase, Mail } from 'lucide-react'
 import { Events, Link, scrollSpy } from 'react-scroll'
 import React, { useEffect } from 'react'
 import { useSection } from '../context/Context'
+import { gsap } from 'gsap'
 import './Aside.css'
 
 
@@ -15,7 +16,7 @@ interface SectionProps {
 const sectionData = [
   { id: 'home', icon: Home, label: 'Home' },
   { id: 'about', icon: User, label: 'About' },
-  { id: 'portfolio', icon: Briefcase, label: 'portfolio' },
+  { id: 'portfolio', icon: Briefcase, label: 'Portfolio' },
   { id: 'contact', icon: Mail, label: 'Contact' }
 ]
 
@@ -26,9 +27,9 @@ const Section: React.FC<SectionProps> = ({ id, icon: Icon, label, isSelected }) 
       key={id}
     >
       {isSelected ? (
-        <div>
+        <div className={`letter-container letter-container-${id}`} >
           {label.split('').map((letter, index) => (
-            <span key={index}>
+            <span key={index} className={`letter-${id}`}>
               {letter}
             </span>
           ))}
@@ -48,15 +49,20 @@ const Aside = () => {
     return () => Events.scrollEvent.remove('end')
   }, [])
 
+  useEffect(() => {
+    if (selectedSection) {
+      gsap.fromTo(
+        `.letter-container-${selectedSection}`,
+        { scale: 0 },
+        { scale: 1, duration: 0.3, ease: "expo.out" }
+      );
+    }
+  }, [selectedSection]);
 
-  const handleSetActive = (id: string) => {
-    setSelectedSection(id)
-  }
-  
   return (
     <div id="aside" >
       {sectionData.map((section) => (
-        <Link to={section.id} key={`${section.id}link`} smooth='true' spy={true} onSetActive={() => handleSetActive(section.id)}>
+        <Link to={section.id} key={`${section.id}link`} smooth='true' spy={true} onSetActive={(id) => setSelectedSection(id)}>
           <Section
             key={section.id}
             {...section}
