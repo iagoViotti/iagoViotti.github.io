63c87d3e858ef8faa7cb1d313d2b44d5881da10a
Iago Viotti <iago.viotti@gmail.com>
2024-11-14 17:28:21 -0300

    :construction: feat about 1st overview

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,18 +3,21 @@ import './About.css'
 const About = () => {
   return (
     <div className="about-container">
-      <div className="text-container">
-        <h1>Iago Viotti</h1>
-        <span>
-          I am a full stack developer with experience in React, Node.js, and Express. I am passionate about creating clean, user-friendly applications that solve real-world problems.
-        </span>
-      </div>
       <div className="photo-container">
-        <img
-          src="https://avatars.githubusercontent.com/u/62115638?v=4"
-          alt="profile"
-          className="photo"
-        />
+        <div className="photo-border">
+          <img
+            src="pfp_portfolio.JPG"
+            alt="profile"
+            className="photo"
+          />
+        </div>
+      </div>
+      <div className="text-container">
+        <span>
+          {/* placeholder text */}
+          I am a full stack developer with experience in React, Node.js, and Express. I am passionate about creating clean, user-friendly applications that solve real-world problems.
+          {/* placeholder text */}
+        </span>
       </div>
     </div>
   )
