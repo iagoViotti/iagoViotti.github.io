24375ee4776fdb917e54cb27b53282e27bd89ad9
Iago Viotti <iago.viotti@gmail.com>
2025-05-02 13:18:38 -0300

    :heavy_plus_sign: build install react-draggable (drag and drop lib)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-function App() {
+import Draggable from "react-draggable"
+
+const App = () => {
   return (
-    <div className='app'>
-    </div >
+    <div className="App">
+      <h1>Welcome to the App</h1>
+      <p>This is a simple React application.</p>
+      <Draggable>
+        <div className="draggable">
+          <h2>Drag me around!</h2>
+          <p>You can drag this element anywhere on the screen.</p>
+        </div>
+      </Draggable>
+    </div>
   )
 }
 
