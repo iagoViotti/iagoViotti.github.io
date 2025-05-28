3c04c63f73832cfcb1daa981a29d8be04d5d7837
Iago Viotti <iago.viotti@gmail.com>
2025-05-28 08:32:54 -0300

    :sparkles: resizable columns working properly, missing th styling

--- src/components/OpenedFolder.tsx
+++ src/components/OpenedFolder.tsx
@@ -23,13 +23,14 @@ const mockData: Project[] = [
   },
 ]
 
-const columns = ['Name', 'Type', 'Description', 'Year', 'Image', 'External Link'];
+const columns = ['Name', 'Type', 'Description', 'Year', 'External Link'];
 
 const OpenedFolder = () => {
   const { doubleClicked, setDoubleClicked } = useSelect()
-  const [columnWidths, setColumnWidths] = useState<number[]>([200, 200, 200, 100, 200, 200]);
+  const [columnWidths, setColumnWidths] = useState<number[]>([200, 200, 200, 100, 200]);
   const currentColIndex = useRef<number | null>(null);
   const isResizing = useRef(false)
+  const [isDragging, setIsDragging] = useState(false);
 
   const handleMouseDown = (index: number) => {
     isResizing.current = true;
@@ -57,15 +58,15 @@ const OpenedFolder = () => {
   }
 
   const getOffsetLeft = (index: number): number => {
-    const col = document.querySelector(`.column[data-index="${index}"]`);
+    const col = document.querySelector(`.column-header-cell[data-index="${index}"]`);
     return col ? (col as HTMLElement).getBoundingClientRect().left : 0;
   };
 
   if (!doubleClicked) return null;
 
   return (
-    <Draggable bounds={'body'} handle=".opened-folder-header">
-      <div className="opened-folder">
+    <Draggable bounds={'body'} handle=".opened-folder-header" onStart={() => setIsDragging(true)} onStop={() => setIsDragging(false)}>
+      <div className={`opened-folder ${isDragging ? 'dragging' : ''}`}>
         <div className="opened-folder-header">
           <div className="opened-folder-header-title">{doubleClicked}</div>
           <button
@@ -75,37 +76,39 @@ const OpenedFolder = () => {
             X
           </button>
         </div>
-        {/* <div className='opened-folder-main'> */}
-          <div className='opened-folder-column-header'>
-            {columns.map((col, index) => (
-              <div
-                key={index}
-                className="column"
-                data-index={index}
-                style={{ width: columnWidths[index] }}
-              >
-                <p>{col}</p>
+        <div className='scrollable-content'>
+          <div className='grid-container'>
+            <div className="opened-folder-column-header">
+              {columns.map((col, index) => (
                 <div
-                  className="resizer"
-                  onMouseDown={() => handleMouseDown(index)}
-                />
-              </div>
-            ))}
-          </div>
-          <div className="opened-folder-content">
-            {mockData.map((item) => (
-              <div key={item.name} className="opened-folder-content-items">
-                <div className="opened-folder-column-item" style={{ width: columnWidths[0] }}><p>{item.name}</p></div>
-                <div className="opened-folder-column-item" style={{ width: columnWidths[1] }}><p>{item.type}</p></div>
-                <div className="opened-folder-column-item" style={{ width: columnWidths[2] }}><p>{item.description}</p></div>
-                <div className="opened-folder-column-item" style={{ width: columnWidths[3] }}><p>{item.year}</p></div>
-                <div className="opened-folder-column-item" style={{ width: columnWidths[4] }}><p>{item.image}</p></div>
-                <div className="opened-folder-column-item" style={{ width: columnWidths[5] }}><p>{item.externalLink}</p></div>
-              </div>
-            ))}
+                  key={index}
+                  className="column-header-cell"
+                  data-index={index}
+                  style={{ width: columnWidths[index] }}
+                >
+                  <p>{col}</p>
+                  <div
+                    className="resizer"
+                    onMouseDown={() => handleMouseDown(index)}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="opened-folder-content-items">
+              {mockData.map((item) => (
+                <div key={item.name} className="opened-folder-content-items">
+                  <div className="column-item-cell" style={{ width: columnWidths[0] }}><p>{item.name}</p></div>
+                  <div className="column-item-cell" style={{ width: columnWidths[1] }}><p>{item.type}</p></div>
+                  <div className="column-item-cell" style={{ width: columnWidths[2] }}><p>{item.description}</p></div>
+                  <div className="column-item-cell" style={{ width: columnWidths[3] }}><p>{item.year}</p></div>
+                  {/* <div className="column-item-cell" style={{ width: columnWidths[4] }}><p>{item.image}</p></div> */}
+                  <div className="column-item-cell" style={{ width: columnWidths[4] }}><p>{item.externalLink}</p></div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      {/* </div> */}
+      </div>
     </Draggable>
   )
 }
