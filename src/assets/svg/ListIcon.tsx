94950c2233bbfc72845a5b41899b2ea72592133f
Iago Viotti <iago.viotti@gmail.com>
2025-06-09 10:01:25 -0300

    :lipstick: feat openedFolder view icons

--- src/assets/svg/ListIcon.tsx
+++ src/assets/svg/ListIcon.tsx
@@ -8,6 +8,6 @@ export const listIcon = (
     strokeWidth="1"
     className="list-icon"
   >
-    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 6L21 6.00078M8 12L21 12.0008M8 18L21 18.0007M3 6.5H4V5.5H3V6.5ZM3 12.5H4V11.5H3V12.5ZM3 18.5H4V17.5H3V18.5Z" stroke="#000000" strokeWidth="2" stroke-linecap="round" strokeLinejoin="round"></path></g>
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 6L21 6.00078M8 12L21 12.0008M8 18L21 18.0007M3 6.5H4V5.5H3V6.5ZM3 12.5H4V11.5H3V12.5ZM3 18.5H4V17.5H3V18.5Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></g>
   </svg>
 )
